77f3486b2b6d67bb9db7513170d45e7087576c0f
Rodrigo Toledo <tito.925.toledo@gmail.com>
2019-04-17 02:33:00 -0400

    added toast/toaster animation; added different levels of toastiness based on score; added pre-game countdown and game duration; changed font-size to be measured in vw for easier responsiveness

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-var clickSpeed = 250;
-var clickCount = 0; // use to calculate cliks per 250ms intervals
-var subCount = 0; // displays number of clicks; used to determine if user still clicking
+var clickSpeed = 250; //
+var currentClicks = 0; // used to calculate cliks per 250ms intervals
+var totalClicks = 0; // used to determine if user still clicking after 250ms
 var allowClickCalc = true; // boolean that prohibits calculation of clickspeed if calculation already ongoing
 var score = 0;
-var playable = true;
+var playable = false;
 
 $(document).ready(function() {
   var burnButton = $("#burn-toast");
@@ -13,13 +13,21 @@ $(document).ready(function() {
   $("#start").on("click",function(){
     score = 0;
     $("#sub-count").html("");
-    $(".cover-game,#start").hide();
-    $("#toaster").addClass("toasting");
+    $(".cover-game").hide();
+    setTimeout(function(){
+      $("#toast-slice,#toaster").addClass("toasting");
+    },1100);
     var timerId = setInterval(countdown, 1000);
-    timeLeft = 30;
+    timeLeft = 3;
     var timerDisplay = $("#timer");
+
+    // pre-game countdown and game duration
     function countdown() {
-      if (timeLeft==0) {
+      if (timeLeft==0 && !playable) {
+        timerDisplay.html("GO!");
+        timeLeft = 30;
+        playable = true;
+      } else if(timeLeft==0 && playable) {
         clearTimeout(timerId);
         endGame(burnButton,scoreText,toasty,speedGaugeBars);
       } else {
@@ -30,21 +38,21 @@ $(document).ready(function() {
   });
   $("#burn-toast").on("click",function(){
     if(playable){
-      subCount++;
+      totalClicks++;
       calculateClickSpeed();
-      showClickSpeed(subCount);
+      showClickSpeed(totalClicks);
       animateScore(burnButton,scoreText,toasty);
     }
   });
 });
 
 function calculateClickSpeed() {
-  clickCount++;
+  currentClicks++;
   if(allowClickCalc) {
     allowClickCalc = false;
     setTimeout(function(){
-      clickSpeed = (clickCount*250)<=1000 ? clickCount*250 : 1000;
-      clickCount = 0;
+      clickSpeed = (currentClicks*250)<=1000 ? currentClicks*250 : 1000;
+      currentClicks = 0;
       allowClickCalc = true;
     },250);
   }
@@ -56,12 +64,19 @@ function showClickSpeed(clickGhost) {
   score = (currentSpeed<4) ? score+currentSpeed : score+10;
   document.getElementById("sub-count").innerHTML = "+"+ score;
   console.log(score +"\t"+ currentSpeed);
-  document.getElementById("multiplier").innerHTML = (currentSpeed<4) ? "x"+ currentSpeed : "x10";
+  var multiplier = document.getElementById("multiplier");
+  if(currentSpeed<4 && currentSpeed>1) {
+    multiplier.innerHTML = "x"+ currentSpeed;
+  } else if(currentSpeed<=1) {
+    multiplier.innerHTML = "";
+  } else {
+    multiplier.innerHTML = "x10";
+  }
   switch(true) {
     case (currentSpeed>=4):
       clickGauge = $("#speed4");
       illuminateSpeedGauge(clickGauge,currentSpeed);
-      clickGauge.children("img").attr("src","img/flaming-toast-light.png");
+      clickGauge.children("img").attr("src","img/flaming-toast-light-multiplier.png");
     case (currentSpeed==3):
       clickGauge = $("#speed3");
       illuminateSpeedGauge(clickGauge,currentSpeed);
@@ -75,9 +90,10 @@ function showClickSpeed(clickGhost) {
   }
 
   setTimeout(function(){
-    if(clickGhost==subCount) {
+    if(clickGhost==totalClicks) {
       $(".speed-gauge-bar").removeClass("lit-click-speed");
-      $("#speed4").attr("src","img/toasty.png");
+      $("#speed4").attr("src","img/toasty-multiplier.png");
+      $("#speed4 p").html("");
       clickSpeed=250;
     }
   },clickSpeed);
@@ -85,7 +101,7 @@ function showClickSpeed(clickGhost) {
 
 function illuminateSpeedGauge(glow,x) {
   dim = $(".speed-gauge-bar:gt("+(x-1)+")");
-  $(".speed-gauge-bar:gt("+(x-1)+")#speed4").children("img").attr("src","img/toasty.png");
+  $(".speed-gauge-bar:gt("+(x-1)+")#speed4").children("img").attr("src","img/toasty-multiplier.png");
   dim.removeClass("lit-click-speed");
   glow.addClass("lit-click-speed");
 }
@@ -109,15 +125,29 @@ function endGame(burnButton,scoreText,toasty,dim) {
   dim.removeClass("lit-click-speed");
   scoreText.removeClass("big-text-active");
   toasty.removeClass("toasty-lit-click-speed");
-  $("#toaster").removeClass("toasting");
   toasty.attr("src","img/toasty.png");
-  $("#rules").hide();
-  $(".cover-game").show();
   var timer = $("#timer");
   timer.html("time's up");
   timer.attr("style","z-index:6");
+
+  var toastSlice = $("#toast-slice");
+  switch(true) {
+    case (score>=2500):
+      toastSlice.addClass("burnt-toast");
+      break;
+    case (score>=1000):
+      toastSlice.addClass("dark-toast");
+      break;
+    case (score>=500):
+      toastSlice.addClass("light-toast");
+      break;
+  }
+
   setTimeout(function(){
-    $(".cover-game").hide();
     timer.hide();
   },2000);
+  setTimeout(function(){
+    $("#toast-slice,#toaster").removeClass("toasting");
+    $("#toaster").removeClass("toasting");
+  },2500);
 }
