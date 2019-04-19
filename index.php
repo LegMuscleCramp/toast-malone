bfeccb702f2ed4a0fe814ee515a7f7a7f0d7fad7
Rodrigo Toledo <tito.925.toledo@gmail.com>
2019-04-18 20:09:23 -0400

    changed to index.php; updated css

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>Toast Malone</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="scripts.js"></script>
+  </head>
+  <body oncontextmenu="return false">
+
+    <!-- toast container -->
+    <div class="toast-container">
+      <div class="content-half">
+        <div class="click-speed-bar">
+          <table class="speed-gauge">
+            <tr>
+              <td id="speed1" class="speed-gauge-bar"></td>
+              <td id="speed2" class="speed-gauge-bar"></td>
+              <td id="speed3" class="speed-gauge-bar"></td>
+            </tr>
+          </table>
+          <div id="speed4" class="speed-gauge-bar">
+            <img src="img/toasty-multiplier.png">
+            <span id="multiplier" class="big-text"></span>
+          </div>
+        </div>
+        <span id="user-score" class="big-text"></span><br>
+        <span id="timer" class="big-text"></span>
+      </div>
+      <div class="content-half">
+        <div id="toast-slice">
+          <img src="img/poasty.png">
+        </div>
+        <div id="toaster">
+          <img src="img/toaster.png">
+        </div>
+        <button id="burn-toast" class="big-button red">
+          <span class="big-text">Toast<br>Malone</span>
+        </button>
+      </div>
+    </div>
+    <div class="cover-game">
+      <div class="top-content">
+        <span id="rules" class="big-text">the rules are simple: mash the 'burn toast' button. ready?</span>
+        <br>
+        <button id="start" class="big-button green">
+          <span class="big-text">start</span>
+        </button>
+      </div>
+    </div>
+    <audio id="boii">
+      <source src="audio/boii.mp3" type="audio/mpeg">
+      Your browser does not support the audio element.
+    </audio>
+
+    <!-- user login form -->
+    <div class="form-container hidden">
+      <form id="login-form" action="login.php" method="post">
+        <span id="final-score" class="score-submit-notify"></span>
+        <span class="score-submit-notify">Sign in to submit your score!</span>
+        <span class="input-label">username</span><br>
+        <input type="text" name="username" required><br>
+        <span class="input-label">password</span><br>
+        <input type="password" name="pwd" required>
+        <input type="submit" value="sign in">
+        <span id="register-link" class="form-help-links">Register</span>
+        <span id="forgot-link" class="form-help-links">Forgot password?</span>
+      </form>
+      <form id="register-form" action="register.php" method="post">
+        <span class="input-label">username</span><br>
+        <input type="text" name="username" required><br>
+        <span class="input-label">password</span><br>
+        <input type="password" name="password" required><br>
+        <span class="input-label">security question</span><br>
+        <input type="text" name="question" required><br>
+        <span class="input-label">security answer</span><br>
+        <input type="text" name="answer" required><br>
+        <input type="submit" value="Register">
+        <span id="login-link" class="form-help-links">return to login</span>
+      </form>
+      <form id="forgot-form" action="retrieve.php" method="post">
+        <span class="input-label">username</span><br>
+        <input type="text" name="username" required><br>
+        <span class="input-label">security question</span><br>
+        <input type="text" name="question" required><br>
+        <span class="input-label">security answer</span><br>
+        <input type="text" name="answer" required><br>
+        <input type="submit" value="Submit">
+        <span id="login-link2" class="form-help-links">return to login</span>
+      </form>
+    </div>
+  </body>
+</html>
